1be44fc660cfb316f5909eb0c9ee8ebbe6f1c4be
ekuinox <depkey@me.com>
2022-02-12 00:46:18 +0900

    とりあえずインデックスを置いておく

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,34 @@
-const Index = () => {
-    return <div>hello world</div>;
+import { GetStaticProps } from "next";
+import Link from 'next/link';
+
+export interface Props {
+    ids: Array<string>;
+}
+
+export const getStaticProps: GetStaticProps<Props> = async () => {
+    const { getFacilityIds } = await import('../../lib/facility');
+    const ids = await getFacilityIds();
+
+    return {
+        props: {
+            ids,
+        },
+    };
+};
+
+const Index = ({ ids }: Props) => {
+    return (
+        <div>
+            {ids.map((id) => (
+                <Link
+                    key={id}
+                    href={`/facilities/${id}`}
+                >
+                    {id}
+                </Link>
+            ))}
+        </div>
+    );
 };
 
 export default Index;

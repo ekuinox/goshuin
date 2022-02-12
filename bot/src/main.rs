bf3e34cc97fc3be9f110106cd060add3cc799fc3
ekuinox <depkey@me.com>
2022-02-12 15:00:00 +0900

    なんかまあこういう感じでコミット撃てるっぽいな

---
+++ bot/src/main.rs
@@ -0,0 +1,34 @@
+use octocrab::{Octocrab, params::repos::Reference, models::repos::Object};
+
+#[tokio::main]
+async fn main() -> octocrab::Result<()> {
+    let token = std::env::var("GITHUB_TOKEN").expect("GITHUB_TOKEN env variable is required");
+
+    let octocrab = Octocrab::builder().personal_token(token).build()?;
+
+    let repo = octocrab.repos("rust-lang", "rust").get().await?;
+
+    let refs = octocrab.repos("ekuinox", "goshuin").get_ref(&Reference::Branch("deploy".into())).await?;
+
+    let sha = match refs.object {
+        Object::Commit { sha, .. } => sha,
+        Object::Tag { sha, .. } => sha,
+        _ => { return Ok(()) },
+    };
+
+    let refs = octocrab.repos("ekuinox", "goshuin").create_ref(
+        &Reference::Branch("deploy-1".into()), sha).await;
+
+    println!("{:?}", refs);
+
+    let repo_metrics = octocrab.repos("rust-lang", "rust").get_community_profile_metrics().await?;
+
+    println!(
+        "{} has {} stars and {}% health percentage",
+        repo.full_name.unwrap(),
+        repo.stargazers_count.unwrap_or(0),
+        repo_metrics.health_percentage
+    );
+
+    Ok(())
+}

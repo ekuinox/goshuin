7827b687edd2afd1d8c044c57c19608193380836
ekuinox <depkey@me.com>
2022-02-13 23:40:34 +0900

    いったいった

--- bot/src/main.rs
+++ bot/src/main.rs
@@ -1,4 +1,4 @@
-use octocrab::{Octocrab, params::repos::Reference, models::repos::Object};
+use octocrab::{models::repos::Object, params::repos::Reference, Octocrab};
 
 #[tokio::main]
 async fn main() -> octocrab::Result<()> {
@@ -6,29 +6,59 @@ async fn main() -> octocrab::Result<()> {
 
     let octocrab = Octocrab::builder().personal_token(token).build()?;
 
-    let repo = octocrab.repos("rust-lang", "rust").get().await?;
-
-    let refs = octocrab.repos("ekuinox", "goshuin").get_ref(&Reference::Branch("deploy".into())).await?;
+    let refs = octocrab
+        .repos("ekuinox", "goshuin")
+        .get_ref(&Reference::Branch("deploy-1".into()))
+        .await?;
 
     let sha = match refs.object {
         Object::Commit { sha, .. } => sha,
         Object::Tag { sha, .. } => sha,
-        _ => { return Ok(()) },
+        _ => return Ok(()),
     };
 
-    let refs = octocrab.repos("ekuinox", "goshuin").create_ref(
-        &Reference::Branch("deploy-1".into()), sha).await;
+    let content = octocrab
+        .repos("ekuinox", "goshuin")
+        .get_content()
+        .path("hello_1")
+        .r#ref(sha)
+        .send()
+        .await?;
 
-    println!("{:?}", refs);
+    let item = content.items.first().expect("msg");
+    let mut content = item
+        .content
+        .to_owned()
+        .and_then(|c| {
+            // 改行コードが 60 文字区切りで入っているので消していく
+            let c = c
+                .chars()
+                .into_iter()
+                .filter(|c| *c != '\n')
+                .collect::<String>();
+            let decoded = base64::decode(c);
+            decoded.ok().and_then(|s| String::from_utf8(s).ok())
+        })
+        .unwrap_or_default();
+    println!("{}", content);
 
-    let repo_metrics = octocrab.repos("rust-lang", "rust").get_community_profile_metrics().await?;
+    content += "hello world\n";
 
-    println!(
-        "{} has {} stars and {}% health percentage",
-        repo.full_name.unwrap(),
-        repo.stargazers_count.unwrap_or(0),
-        repo_metrics.health_percentage
-    );
+    let repo = octocrab.repos("ekuinox", "goshuin");
+    let a = repo
+        .update_file("hello_1", "append hello", content, item.sha.to_owned())
+        .branch("deploy-1")
+        .send()
+        .await;
+    println!("{:?}", a);
 
     Ok(())
+}
+
+#[test]
+fn a() {
+    println!("{:?}", base64::encode("hello world").bytes());
+
+    let bytes = base64::decode("aGVsbG8gd29ybGQK").unwrap();
+    println!("{:?}", bytes);
 }

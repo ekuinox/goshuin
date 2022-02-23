ffdcf687b61f02ac4128c3accd542075aa15d115
ekuinox <depkey@me.com>
2022-02-23 15:50:07 +0900

    ちょっといじる

--- bot/src/commands/open_command.rs
+++ bot/src/commands/open_command.rs
@@ -10,8 +10,8 @@ use serenity::{
     prelude::*,
 };
 
-use crate::editor::EditorData;
 use super::get_args;
+use crate::editor::EditorData;
 
 #[command]
 #[required_permissions("ADMINISTRATOR")]
@@ -21,15 +21,22 @@ pub async fn open(ctx: &Context, msg: &Message) -> CommandResult {
 
 pub async fn open_command(ctx: &Context, msg: &Message) -> CommandResult {
     let mut data = ctx.data.write().await;
-    let mut editor = data.get_mut::<EditorData>().expect("Editor is None").lock().await;
-    let args = get_args(&msg.content);
-    let reply= if let Ok(facility) = editor.open(&args[0]).await {
+    let mut editor = data
+        .get_mut::<EditorData>()
+        .expect("Editor is None")
+        .lock()
+        .await;
+    let id = if let Some(id) = msg.content.split(' ').nth(1) {
+        id.to_string()
+    } else {
+        msg.channel_id.say(&ctx.http, "id not specified").await?;
+        return Ok(());
+    };
+    let reply = if let Ok(facility) = editor.open(&id).await {
         format!("{} is found. name = {}", facility.id, facility.name)
     } else {
-        format!("{} is not found.", args[0])
+        format!("{} is not found.", id)
     };
-    msg.channel_id
-        .say(&ctx.http, reply)
-        .await?;
+    msg.channel_id.say(&ctx.http, reply).await?;
     Ok(())
 }

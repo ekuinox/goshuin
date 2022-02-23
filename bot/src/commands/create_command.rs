c370c9e2ee58a091e88b3ddf464976d692537bdc
ekuinox <depkey@me.com>
2022-02-23 15:46:40 +0900

    新しく作成するために

--- bot/src/commands/create_command.rs
+++ bot/src/commands/create_command.rs
@@ -1,12 +1,11 @@
+use crate::{
+    editor::EditorData,
+    facility::{Coordinate, FacilityKind},
+};
+use clap::Parser;
 use serenity::{
-    framework::standard::{
-        buckets::{LimitedFor, RevertBucket},
-        help_commands,
-        macros::{check, command, group, help, hook},
-        Args, CommandGroup, CommandOptions, CommandResult, DispatchError, HelpOptions, Reason,
-        StandardFramework,
-    },
-    model::{channel::Message, id::UserId},
+    framework::standard::{macros::command, CommandResult},
+    model::channel::Message,
     prelude::*,
 };
 
@@ -16,9 +15,52 @@ pub async fn create(ctx: &Context, msg: &Message) -> CommandResult {
     create_command(ctx, msg).await
 }
 
+#[derive(Parser, Debug)]
+struct CreateArgs {
+    pub id: String,
+
+    pub name: String,
+
+    pub kind: FacilityKind,
+
+    pub lat: f64,
+
+    pub lon: f64,
+}
+
 pub async fn create_command(ctx: &Context, msg: &Message) -> CommandResult {
+    let mut data = ctx.data.write().await;
+    let mut editor = data
+        .get_mut::<EditorData>()
+        .expect("Editor is None")
+        .lock()
+        .await;
+    let args = msg
+        .content
+        .split(' ')
+        .map(|s| s.to_string())
+        .collect::<Vec<String>>();
+    let args = CreateArgs::try_parse_from(&args)?;
+
+    let facility = editor.create(
+        args.id,
+        args.name,
+        args.kind,
+        Coordinate::new(args.lat, args.lon),
+    );
+
     msg.channel_id
-        .say(&ctx.http, "create command unimplemented")
+        .say(
+            &ctx.http,
+            format!(
+                "created with id = {}, name = {}, kind = {:?}, lat = {}, lon = {}",
+                facility.id,
+                facility.name,
+                facility.kind,
+                facility.coordinate.lat,
+                facility.coordinate.lon
+            ),
+        )
         .await?;
     Ok(())
 }

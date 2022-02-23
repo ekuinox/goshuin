2f369912f63253d3b25e5f9cb9761f5f62f625c0
ekuinox <depkey@me.com>
2022-02-23 12:42:26 +0900

    wip

--- bot/src/bot.rs
+++ bot/src/bot.rs
@@ -1,6 +1,5 @@
-use crate::cli::Cli;
+use crate::cli::{Cli, Context as CliContext};
 use crate::client::GoshuinRepositoryClient;
-use crate::facility::Facility;
 use clap::Parser;
 use serenity::{
     async_trait,
@@ -10,23 +9,21 @@ use serenity::{
 
 pub struct Handler {
     admin_id: UserId,
-    facility: Option<Facility>,
-    client: GoshuinRepositoryClient,
+    context: CliContext,
 }
 
 impl Handler {
     pub fn new(admin_id: UserId, client: GoshuinRepositoryClient) -> Handler {
         Handler {
-            client,
             admin_id,
-            facility: None,
+            context: CliContext::new(client),
         }
     }
 }
 
 #[async_trait]
 impl EventHandler for Handler {
-    async fn message(&self, ctx: Context, message: Message) {
+    async fn message(&self, _ctx: Context, message: Message) {
         if message.author.id != self.admin_id {
             return;
         }
@@ -45,21 +42,8 @@ impl EventHandler for Handler {
                 return;
             }
         };
-        match cli {
-            Cli::GetSubcommand { id } => {
-                if let Ok(facility) = self.client.get_facility(&id).await {
-                    let json = match serde_json::to_string_pretty(&facility) {
-                        Ok(json) => json,
-                        Err(_) => return,
-                    };
-                    let _ = message.channel_id.say(&ctx.http, json).await;
-                } else {
-                    let _ = message
-                        .channel_id
-                        .say(&ctx.http, format!("{} is not found", id))
-                        .await;
-                }
-            }
+        if let Err(e) = cli.run(&self.context, &message).await {
+            eprintln!("{:#?}", e);
         }
     }
 
--- bot/src/cli.rs
+++ bot/src/cli.rs
@@ -1,10 +1,44 @@
+use crate::client::GoshuinRepositoryClient;
+use anyhow::Result;
 use clap::Parser;
+use serenity::model::channel::Message;
+
+#[derive(Parser, Debug)]
+pub struct EditStartSubcommand {
+    id: String,
+}
+
+impl EditStartSubcommand {
+    async fn run(&self, _ctx: &Context, _message: &Message) -> Result<()> {
+        Ok(())
+    }
+}
 
 #[derive(Parser, Debug)]
 #[clap(about, version, author)]
 pub enum Cli {
     #[clap(name = "get")]
-    GetSubcommand {
-        id: String,
-    },
+    GetSubcommand { id: String },
+    #[clap(name = "edit-start")]
+    EditStartSubcommand(EditStartSubcommand),
+}
+
+pub struct Context {
+    client: GoshuinRepositoryClient,
+}
+
+impl Context {
+    pub fn new(client: GoshuinRepositoryClient) -> Context {
+        Context { client }
+    }
+}
+
+impl Cli {
+    pub async fn run(&self, ctx: &Context, message: &Message) -> Result<()> {
+        let r = match self {
+            Cli::EditStartSubcommand(s) => s.run(ctx, message).await?,
+            _ => {}
+        };
+        Ok(r)
+    }
 }
